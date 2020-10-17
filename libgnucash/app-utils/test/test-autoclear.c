8321fa4628ad9e6a5e1b2b0d83129559338fd246
Cristian Klein <cristian.klein@elastisys.com>
2020-10-17 08:52:19 +0200

    Add auto-clear test boilerplate

--- libgnucash/app-utils/test/test-autoclear.c
+++ libgnucash/app-utils/test/test-autoclear.c
@@ -1,5 +1,5 @@
 /********************************************************************
- * test-app-utils.c: GLib g_test test execution file.		    *
+ * test-autoclear.c: test suite for Auto-Clear          	    *
  * Copyright 2013 John Ralls <jralls@ceridwen.us>		    *
  *                                                                  *
  * This program is free software; you can redistribute it and/or    *
@@ -13,45 +13,48 @@
  * GNU General Public License for more details.                     *
  *                                                                  *
  * You should have received a copy of the GNU General Public License*
- * along with this program; if not, contact:                        *
+ * along with this program; if not, you can retrieve it from        *
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.html            *
+ * or contact:                                                      *
  *                                                                  *
  * Free Software Foundation           Voice:  +1-617-542-5942       *
  * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
  * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
-\********************************************************************/
-
+ ********************************************************************/
 #include <config.h>
 #include <glib.h>
-#include <qof.h>
-#include <libguile.h>
+#include <unittest-support.h>
+#include <qofbookslots.h>
+#include "test-engine-stuff.h"
+#include "../option-util.h"
 
-extern void test_suite_autoclear (void);
-extern void test_suite_option_util (void);
-extern void test_suite_gnc_ui_util (void);
+static const gchar *suitename = "/app-utils/autoclear";
+
+void test_suite_autoclear (void);
+
+typedef struct
+{
+} Fixture;
 
 static void
-guile_main (void *closure, int argc, char **argv)
+setup (Fixture *fixture, gconstpointer pData)
 {
-    int retval;
-    scm_c_use_module("gnucash app-utils");
-
-    test_suite_autoclear ();
-    test_suite_option_util ();
-    test_suite_gnc_ui_util ();
-    retval = g_test_run ();
-
-    exit (retval);
 }
 
-int
-main (int argc, char *argv[])
+static void
+teardown (Fixture *fixture, gconstpointer pData)
 {
-    qof_init (); 			/* Initialize the GObject system */
-    qof_log_init_filename_special ("stderr"); /* Init the log system */
-    g_test_init (&argc, &argv, NULL); 	/* initialize test program */
-    //qof_log_set_level("gnc", G_LOG_LEVEL_DEBUG);
-    g_test_bug_base("https://bugs.gnucash.org/show_bug.cgi?id="); /* init the bugzilla URL */
-    g_setenv ("GNC_UNINSTALLED", "1", TRUE);
-    scm_boot_guile (argc, argv, guile_main, NULL);
-    return 0;
+    test_clear_error_list();
+}
+
+static void
+test_autoclear (Fixture *fixture, gconstpointer pData)
+{
+    g_assert (TRUE);
+}
+
+void
+test_suite_autoclear (void)
+{
+    GNC_TEST_ADD (suitename, "Auto-clear all transactions", Fixture, NULL, setup, test_autoclear, teardown);
 }
